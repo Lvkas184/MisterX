616a4208ede6e5d004192e5b7e4ee9aa95b7a7ef
lukas184 <mail@lukas-heinz.de>
2024-03-31 21:12:05 +0200

    .

---
+++ app/src/main/java/com/lukas/misterx/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lukas.misterx
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.lukas.misterx.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
