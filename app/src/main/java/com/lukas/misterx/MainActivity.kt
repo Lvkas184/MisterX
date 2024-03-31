a810d80f405047f213c905a9ae74e9c1602bad97
Lvkas184 <uxime@student.kit.edu>
2024-03-31 22:40:52 +0200

    .

--- app/src/main/java/com/lukas/misterx/MainActivity.kt
+++ app/src/main/java/com/lukas/misterx/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lukas.misterx
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 import com.lukas.misterx.databinding.ActivityMainBinding
@@ -8,11 +10,21 @@ import com.lukas.misterx.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var button1: Button
+    private lateinit var button2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        button1 = findViewById(R.id.button)
+        button2 = findViewById(R.id.button2)
+
+        button1.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
